b9d2695d382d7ca97061ead3415bf0ab4c1eae7c
Siftal <m.khanmohamadzadeh@gmail.com>
2019-10-19 10:09:38 -0400

    remove settings

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,23 +1,17 @@
-var rpcUrl = "https://rinkeby.infura.io/v3/81a17a01107e4ac9bf8a556da267ae2d";
-// var rpcUrl = "https://rinkeby.infura.io/v3/588bb93634084be69f62f302a279d76f";
+var rpcUrl = "";
 
 var addresses = {
-    pt: "0xDD3a0A73Ff48180f248B953ea7715dAEF35fe812",
-    sp_minter: "0x473482722677ea1625d38891759741120A7c155e",
-    sp: "0x26ac3eE474263232D50F087be8D45E4ad4D83B33",
-    subs_minter: "0xfDC86E3094A894C1f37ee93934D87CC316D59cb9",
-    subs: "0x1B65cccdaC6825546DED0913028a09e5C903824e",
+    pt: "",
+    sp_minter: "",
+    sp: "",
+    subs_minter: "",
+    subs: "",
 };
 
 var abies = {
-    pt:
-        ''
-    sp_minter:
-        ''
-    sp:
-        ''
-    subs_minter:
-        ''
-    subs:
-        ''
+    pt: "",
+    sp_minter: "",
+    sp: "",
+    subs_minter: "",
+    subs: ""
 };
